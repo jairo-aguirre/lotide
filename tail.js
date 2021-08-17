4a83fd47151be720a170668243ea77a9ba847042
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-16 19:30:44 -0700

    Getting the tail

--- tail.js
+++ tail.js
@@ -10,7 +10,7 @@ const tail = function(elements) {
   let newArray = [];
 
   for (let i = 1; i < elements.length; i++) {
-    newArray[i-1] = elements[i];
+    newArray[i - 1] = elements[i];
   }
 
   return newArray;
