c18ac0daf50ea74d4ea9efd3d49d0fdd827cbefb
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-16 18:18:52 -0700

    Assertion test

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion ✅: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion ❌: " + actual + " !=== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Jairo", "Jairo");
+assertEqual(1, 1);
+assertEqual(1, 2);
