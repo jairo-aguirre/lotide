12a1531e140c8b795a00a0fd56cc61360c49bae0
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-17 19:04:29 -0700

    Comparing arrays

--- eqArrays.js
+++ eqArrays.js
@@ -24,6 +24,7 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
+// Test cases
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
