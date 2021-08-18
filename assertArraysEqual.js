7fe460e6ddaa78206e02b49d3c07c8cf54e6149b
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-17 19:26:09 -0700

    Comparing arrays and reusing functions

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  const length = array1.length;
+
+  for (let i = 0; i < length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(assert1, assert2) {
+  if (eqArrays(assert1, assert2)) {
+    console.log(`Assertion ✅: ${assert1} === ${assert2}`);
+  } else {
+    console.log(`Assertion ❌: ${assert1} !=== ${assert2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    // console.log("Assertion ✅: " + actual + " === " + expected);
     console.log(`Assertion ✅: ${actual} === ${expected}`);
   } else {
-    // console.log("Assertion ❌: " + actual + " !=== " + expected);
     console.log(`Assertion ❌: ${actual} !=== ${expected}`);
   }
 };
