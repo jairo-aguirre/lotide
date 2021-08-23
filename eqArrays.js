d75bbd11b0223b94f75861ea36aab8ae6aaa37a0
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-23 12:56:58 -0700

    Refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion ✅: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion ❌: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
+const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -22,8 +14,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
