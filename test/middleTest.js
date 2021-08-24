355d016cfa7fb4ea8da26fa811bf37f7939615c9
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-23 17:43:08 -0700

    Refactoring tail and middle with M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,11 +3,9 @@ const middle = require('../middle');
 
 // Test Code
 const numbers = [1, 2, 3, 4, 5];
-middle(numbers);
-// assertArraysEqual(numbers, [1, 2, 3, 4, 5]);
 
-describe('#head', () => {
-  it('returns true when is compare numbers length with [1, 2, 3, 4, 5] length', () => {
-    assertArraysEqual.strictEqual(numbers.length, [1, 2, 3, 4, 5].length);
+describe('#middle', () => {
+  it('returns the middle element in an array', () => {
+    assertArraysEqual.deepEqual(middle(numbers), [3]);
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -2,11 +2,9 @@ const assertEqual = require('chai').assert;
 const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// // assertEqual(words.length, 3); // original array should still have 3 elements!
 
 describe('#tail', () => {
-  it('returns the lenght of the word array wich is 3', () => {
-    assertEqual.equal(words.length, 3);
+  it('returns the tail of an array', () => {
+    assertEqual.deepEqual(tail(words), ["Lighthouse", "Labs"]);
   });
 });
